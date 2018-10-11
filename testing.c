8aaa4f0838c7fbf7b9bc09128840361c70175085
huiminwu0 <huiminwu24@gmail.com>
2018-10-10 20:18:51 -0400

    Update with classwork

--- testing.c
+++ testing.c
@@ -3,18 +3,15 @@
 #include "list.h"
 
 int main() {
-    struct node * n1 = malloc(sizeof(struct node));
-    struct node * n2 = malloc(sizeof(struct node));
-    struct node * n3 = malloc(sizeof(struct node));
+    struct node * n1 = NULL;//malloc(sizeof(struct node));
 
-    n1->i = 6;
-    n1->next = n2;
-    n2->i = 94;
-    n2->next = n3;
-    n3->i = 310;
     print_list(n1);
 
-    print_list(insert_front(n1, 479));
+    n1 = insert_front(n1, 479);
+    print_list(n1);
+
+    n1 = insert_front(n1, 310);
+    print_list(n1);
 
     print_list(free_list(n1));
     return 0;
